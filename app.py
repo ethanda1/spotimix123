3f74ecb967d95f32188a78bef0e2748bf6b72b92
savii <savii462@gmail.com>
2024-07-05 01:28:53 -0500

    tweake for render

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ CORS(app)
 app.config['SECRET_KEY'] = os.urandom(64)
 
 sp_oauth = SpotifyOAuth(
-    client_id='SPOTIPY_CLIENT_ID',
-    client_secret='SPOTIPY_CLIENT_SECRET',
-    redirect_uri='https://spotimix123.onrender.com/callback',
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+    redirect_uri=os.getenv('https://spotimix123.onrender.com/callback'),
     scope='playlist-modify-public user-top-read'
 )
 
